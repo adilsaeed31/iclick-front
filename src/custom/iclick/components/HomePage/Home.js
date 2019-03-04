e4b07a26c55afd5adb9f07dfac09bdd852341124
Muhammad Adil <adil.1063@awok.ae>
2019-03-04 17:07:37 +0400

    Added new logo

--- src/custom/iclick/components/HomePage/Home.js
+++ src/custom/iclick/components/HomePage/Home.js
@@ -1,27 +1,27 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
+import React from "react"
+import PropTypes from "prop-types"
+import { withStyles } from "@material-ui/core/styles"
+import Typography from "@material-ui/core/Typography"
 
-const styles = (theme) => ({
+const styles = theme => ({
   homepage: {
     alignItems: "center",
     display: "flex",
     justifyContent: "center",
     marginBottom: theme.spacing.unit * 2
   }
-});
+})
 
 const HomePage = () => (
   <Typography variant="caption">
     <div className="homepage">
-      <p>HomePage</p>
+      <p>Home Page</p>
     </div>
   </Typography>
-);
+)
 
 HomePage.propTypes = {
   classes: PropTypes.object
-};
+}
 
-export default withStyles(styles)(HomePage);
+export default withStyles(styles)(HomePage)
