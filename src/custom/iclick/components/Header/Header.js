cda2fe29ea4c4035069c6aabf2178c4c1feb9978
Muhammad Adil <adil.1063@awok.ae>
2019-03-14 17:52:21 +0400

    created old header

--- src/custom/iclick/components/Header/Header.js
+++ src/custom/iclick/components/Header/Header.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { NavigationDesktop } from "custom/iclick/components/NavigationDesktop";
-import TopHeader from "custom/iclick/components/TopHeader";
-import MiddleHeader from "custom/iclick/components/MiddleHeader";
+import AccountDropdown from "components/AccountDropdown";
+import Logo from "custom/iclick/components/MiddleHeader/Logo";
+import CartDropDown from "custom/iclick/components/MiddleHeader/CartDropDown";
 
 class Header extends Component {
   static propTypes = {
@@ -11,71 +12,19 @@ class Header extends Component {
     }).isRequired
   }
 
-  state = {
-    headerCls: "header-bottom sticky-header"
-  }
-
-  _mounted = false
-
-  componentDidMount() {
-    this._mounted = true;
-    window.addEventListener("scroll", this.handleScroll, { passive: true });
-  }
-
-  componentWillUnmount() {
-    this._mounted = false;
-    window.removeEventListener("scroll", this.handleScroll, { passive: true });
-  }
-
-  handleScroll = () => {
-    if (this._mounted) {
-      if (this.scrollCalc() >= 8) {
-        this.setState({ headerCls: "header-bottom sticky-header fixed" });
-        return;
-      }
-
-      this.setState({ headerCls: "header-bottom sticky-header" });
-    }
-  }
-
-  scrollCalc = () => {
-    const getDocHeight = () => {
-      const getDOM = document;
-      return Math.max(
-        getDOM.body.scrollHeight,
-        getDOM.documentElement.scrollHeight,
-        getDOM.body.offsetHeight,
-        getDOM.documentElement.offsetHeight,
-        getDOM.body.clientHeight,
-        getDOM.documentElement.clientHeight
-      );
-    };
-
-    const winHeight = window.innerHeight || (document.documentElement || document.body).clientHeight;
-    const docHeight = getDocHeight();
-    const scrollTop =
-      window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    const trackLength = docHeight - winHeight;
-    const pctScrolled = Math.floor((scrollTop / trackLength) * 100);
-
-    return pctScrolled === undefined ? null : pctScrolled;
-  }
-
   render() {
     const { shop } = this.props;
-    const { headerCls } = this.state;
     return (
       <header className="header">
-        <div className="header-top">
-          <TopHeader />
-        </div>
-        <div className="header-middle">
-          <MiddleHeader shop={shop} />
-        </div>
         <div className="sticky-wrapper">
-          <div className={headerCls}>
-            <div className="container">
-              <NavigationDesktop shop={shop} />
+          <div className="header-bottom sticky-header fixed">
+            <div className="container wide-header">
+              <Logo shop={shop} />
+              <div className="menu-bar">
+                <NavigationDesktop />
+              </div>
+              <CartDropDown />
+              <AccountDropdown />
             </div>
           </div>
         </div>
--- src/custom/iclick/components/NavigationDesktop/NavigationDesktop.js
+++ src/custom/iclick/components/NavigationDesktop/NavigationDesktop.js
@@ -1,17 +1,12 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { inject } from "mobx-react";
 import { NavigationItemDesktop } from "custom/iclick/components/NavigationDesktop";
-import Logo from "custom/iclick/components/MiddleHeader/Logo";
-import CartDropDown from "custom/iclick/components/MiddleHeader/CartDropDown";
 
 @inject("navItems")
 export class NavigationDesktop extends Component {
   static propTypes = {
-    navItems: PropTypes.array,
-    shop: PropTypes.shape({
-      name: PropTypes.string
-    }).isRequired
+    navItems: PropTypes.array
   }
 
   static defaultProps = {
@@ -23,16 +18,12 @@ export class NavigationDesktop extends Component {
   }
 
   render() {
-    const { navItems, shop } = this.props;
+    const { navItems } = this.props;
 
     return (
-      <Fragment>
-        <Logo shop={shop} />
-        <CartDropDown />
-        <nav className="main-nav">
-          <ul className="menu sf-arrows sf-js-enabled">{navItems.map(this.renderNavItem)}</ul>
-        </nav>
-      </Fragment>
+      <nav className="main-nav">
+        <ul className="menu sf-arrows sf-js-enabled">{navItems.map(this.renderNavItem)}</ul>
+      </nav>
     );
   }
 }
