fefc68e3d0f74ac412933972de9ad435fdf2c3ed
Muhammad Adil <adil.1063@awok.ae>
2019-03-24 14:47:46 +0400

    Update lat/lng

--- src/custom/iclick/components/FooterMaps/FooterMaps.js
+++ src/custom/iclick/components/FooterMaps/FooterMaps.js
@@ -1,7 +1,7 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { compose, withProps } from "recompose";
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import React from "react"
+import PropTypes from "prop-types"
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 const FooterMaps = compose(
   withProps({
@@ -13,18 +13,18 @@ const FooterMaps = compose(
   }),
   withScriptjs,
   withGoogleMap
-)((props) => (
-  // 25.2640948,55.3056882 iClick Lat/Lng
-  <GoogleMap defaultZoom={18} defaultCenter={{ lat: 25.2640948, lng: 55.3056882 }}>
-    {props.isMarkerShown && <Marker position={{ lat: 25.2640948, lng: 55.3056882 }} />}
+)(props => (
+  // 25.2641412,55.3057765 iClick Lat/Lng
+  <GoogleMap defaultZoom={20.12} defaultCenter={{ lat: 25.2641412, lng: 55.3057765 }}>
+    {props.isMarkerShown && <Marker position={{ lat: 25.2641412, lng: 55.3057765 }} />}
   </GoogleMap>
-));
+))
 
 FooterMaps.defaultProps = {
   isMarkerShown: true
-};
+}
 
 FooterMaps.propTypes = {
   isMarkerShown: PropTypes.bool.isRequired
-};
-export default FooterMaps;
+}
+export default FooterMaps
