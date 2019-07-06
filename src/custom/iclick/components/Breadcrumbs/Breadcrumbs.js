2f1eeae151795d3f816738e56b00979371c942a1
Muhammad Adil <adilsaeed31@gmail.com>
2019-07-06 12:39:51 +0300

    Added breadcrumbs

--- src/custom/iclick/components/Breadcrumbs/Breadcrumbs.js
+++ src/custom/iclick/components/Breadcrumbs/Breadcrumbs.js
@@ -1,9 +1,9 @@
-import React, { Component, Fragment } from "react";
-import PropTypes from "prop-types";
-import { inject } from "mobx-react";
-import ChevronRight from "mdi-material-ui/ChevronRight";
-import Link from "components/Link";
-import SharedPropTypes from "lib/utils/SharedPropTypes";
+import React, { Component, Fragment } from "react"
+import PropTypes from "prop-types"
+import { inject } from "mobx-react"
+import ChevronRight from "mdi-material-ui/ChevronRight"
+import Link from "components/Link"
+import SharedPropTypes from "lib/utils/SharedPropTypes"
 
 @inject("tags")
 class Breadcrumbs extends Component {
@@ -16,84 +16,93 @@ class Breadcrumbs extends Component {
   }
 
   renderTagBreadcrumbPiece(tag) {
-    const { tags } = this.props;
+    const { tags } = this.props
 
     // Find first tag that is a parent of this tag, if any are
-    const parentTag = tags.find((node) => node.subTagIds.includes(tag._id));
+    const parentTag = tags.find(node => node.subTagIds.includes(tag._id))
 
     return (
       <Fragment>
         {!!parentTag && this.renderTagBreadcrumbPiece(parentTag)}
-        <ChevronRight />
-        <Link route={`/tag/${tag.slug}`}>
-          <span>{tag.name}</span>
-        </Link>
+        <li class="breadcrumb-item">
+          <Link route={`/tag/${tag.slug}`}>
+            <span>{tag.name}</span>
+          </Link>
+        </li>
       </Fragment>
-    );
+    )
   }
 
   renderTagBreadcrumbs() {
-    const { tagId, tags } = this.props;
+    const { tagId, tags } = this.props
 
-    if (!tagId || !Array.isArray(tags) || tags.length === 0) return null; // still loading
+    if (!tagId || !Array.isArray(tags) || tags.length === 0) return null // still loading
 
-    const currentTag = tags.find((tag) => tag._id === tagId);
+    const currentTag = tags.find(tag => tag._id === tagId)
     if (!currentTag) {
-      throw new Error(`Unable to find current tag with ID ${tagId}`);
+      throw new Error(`Unable to find current tag with ID ${tagId}`)
     }
 
-    return this.renderTagBreadcrumbPiece(currentTag);
+    return this.renderTagBreadcrumbPiece(currentTag)
   }
 
   renderProductNameBreadcrumb = () => {
-    const { product, tagId } = this.props;
+    const { product, tagId } = this.props
 
     if (tagId) {
       return (
         <Fragment>
           {this.renderTagBreadcrumbs()}
-          <ChevronRight />
-          <Link route={`/product/${product.slug}`}>
-            <span>{product.title}</span>
-          </Link>
+          <li class="breadcrumb-item">
+            <Link route={`/product/${product.slug}`}>
+              <span>{tag.name}</span>
+            </Link>
+          </li>
         </Fragment>
-      );
+      )
     }
 
     return (
       <Fragment>
-        <ChevronRight />
-        <Link route={`/product/${product.slug}`}>
-          <span>{product.title}</span>
-        </Link>
+        <li class="breadcrumb-item">
+          <Link route={`/product/${product.slug}`}>
+            <span>{product.title}</span>
+          </Link>
+        </li>
       </Fragment>
-    );
+    )
   }
 
   renderBreadcrumbs() {
-    const { isPDP, isTagGrid } = this.props;
+    const { isPDP, isTagGrid } = this.props
 
     if (isTagGrid) {
-      return this.renderTagBreadcrumbs();
+      return this.renderTagBreadcrumbs()
     }
 
     if (isPDP) {
-      return this.renderProductNameBreadcrumb();
+      return this.renderProductNameBreadcrumb()
     }
 
-    return null;
+    return null
   }
 
   render() {
     return (
-      <div>
-        <Link route="/">
-          <span>Home</span>
-        </Link>
-        {this.renderBreadcrumbs()}
-      </div>
-    );
+      <nav aria-label="breadcrumb" class="breadcrumb-nav mb-0">
+        <div class="container">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <Link route="/">
+                <i class="icon-home" />
+              </Link>
+            </li>
+            {this.renderBreadcrumbs()}
+          </ol>
+        </div>
+      </nav>
+    )
   }
 }
 
-export default Breadcrumbs;
+export default Breadcrumbs
--- src/custom/iclick/components/MiniCart/MiniCart.js
+++ src/custom/iclick/components/MiniCart/MiniCart.js
@@ -1,18 +1,16 @@
-import React, {Component} from "react";
+import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
-import {inject, observer} from "mobx-react";
-import MiniCartComponent from "@reactioncommerce/components/MiniCart/v1";
-import CartItems from "components/CartItems";
-import CartEmptyMessage from "@reactioncommerce/components/CartEmptyMessage/v1";
-import {Router} from "routes";
+import { inject, observer } from "mobx-react";
+import { Router } from "routes";
 import withCart from "containers/cart/withCart";
 import withShop from "containers/shop/withShop";
 import trackCartItems from "lib/tracking/trackCartItems";
 import track from "lib/tracking/track";
 import variantById from "lib/utils/variantById";
 import TRACKING from "lib/tracking/constants";
+import Link from "custom/iclick/components/Link";
 
-const {CART_VIEWED, PRODUCT_REMOVED} = TRACKING;
+const { CART_VIEWED, PRODUCT_REMOVED } = TRACKING;
 
 @withShop
 @withCart
@@ -41,195 +39,156 @@ export default class MiniCart extends Component {
       openCart: PropTypes.func.isRequired,
       closeCart: PropTypes.func.isRequired
     })
-  }
+  };
 
   constructor(props) {
     super(props);
 
-    this.setPopoverAnchorEl = (element) => {
+    this.setPopoverAnchorEl = element => {
       this.anchorElement = element;
     };
   }
 
   state = {
     anchorElement: null
-  }
+  };
 
-  anchorElement = null
+  anchorElement = null;
 
   handlePopperOpen = () => {
     const {
       cart,
-      uiStore: {openCart}
+      uiStore: { openCart }
     } = this.props;
     openCart();
 
     // Track a cart view event, only if the cart contains items
     if (cart && Array.isArray(cart.items) && cart.items.length) {
-      this.trackAction({cartItems: cart.items, cartId: cart._id, action: CART_VIEWED});
+      this.trackAction({ cartItems: cart.items, cartId: cart._id, action: CART_VIEWED });
     }
-  }
+  };
 
-  handleClick = () => Router.pushRoute("/")
+  handleClick = () => Router.pushRoute("/");
 
   handleCheckoutButtonClick = () => {
     this.handleLeavePopper();
     Router.pushRoute("/cart/checkout");
-  }
-
-  handlePopperClose = () => {
-    const {closeCart} = this.props.uiStore;
-    closeCart(0);
-  }
-
-  handleEnterPopper = () => {
-    const {openCart} = this.props.uiStore;
-    openCart();
-  }
-
-  handleLeavePopper = () => {
-    const {closeCart} = this.props.uiStore;
-    closeCart();
-  }
+  };
 
   handleOnClick = () => {
-    const {closeCart} = this.props.uiStore;
+    const { closeCart } = this.props.uiStore;
     closeCart();
     Router.pushRoute("cart");
-  }
+  };
 
   handleItemQuantityChange = (quantity, cartItemId) => {
-    const {onChangeCartItemsQuantity} = this.props;
+    const { onChangeCartItemsQuantity } = this.props;
 
-    onChangeCartItemsQuantity({quantity, cartItemId});
-  }
+    onChangeCartItemsQuantity({ quantity, cartItemId });
+  };
 
   @trackCartItems()
-  trackAction() {
-  }
+  trackAction() {}
 
-  handleRemoveItem = async (itemId) => {
+  handleRemoveItem = async itemId => {
+    console.log(itemId, "itemId");
     const {
-      cart: {items},
+      cart: { items },
       onRemoveCartItems
     } = this.props;
-    const {data, error} = await onRemoveCartItems(itemId);
 
+    const { data, error } = await onRemoveCartItems(itemId);
     if (data && !error) {
       const {
-        cart: {_id}
+        cart: { _id }
       } = data.removeCartItems;
-      const removedItem = {cart_id: _id, ...variantById(items, itemId)}; // eslint-disable-line camelcase
+      const removedItem = { cart_id: _id, ...variantById(items, itemId) }; // eslint-disable-line camelcase
 
       // Track removed item
-      this.trackAction({cartItems: removedItem, action: PRODUCT_REMOVED});
+      this.trackAction({ cartItems: removedItem, action: PRODUCT_REMOVED });
     }
-  }
-
-  renderMiniCart() {
-    const {cart, hasMoreCartItems, loadMoreCartItems} = this.props;
-
-    if (cart && Array.isArray(cart.items) && cart.items.length) {
-      return (
-        <MiniCartComponent
-          cart={cart}
-          onCheckoutButtonClick={this.handleCheckoutButtonClick}
-          components={{
-            QuantityInput: "div",
-            CartItems: (cartItemProps) => (
-              <CartItems
-                {...cartItemProps}
-                hasMoreCartItems={hasMoreCartItems}
-                onRemoveItemFromCart={this.handleRemoveItem}
-                onChangeCartItemQuantity={this.handleItemQuantityChange}
-                onLoadMoreCartItems={loadMoreCartItems}
-              />
-            )
-          }}
-        />
-      );
-    }
-
-    return (
-      <div>
-        <CartEmptyMessage onClick={this.handleClick}/>
-      </div>
-    );
-  }
+  };
 
   render() {
-    const {cart, uiStore} = this.props;
-    const {isCartOpen} = uiStore;
-    const id = isCartOpen ? "simple-popper" : null;
-    let total = 0;
-    if (cart) {
-      total = cart.items.reduce((accumulator, item) => {
-        return accumulator + (item.price.amount * item.quantity)
-      }, 0);
-    }
+    const { cart } = this.props;
+
     return (
       <div className="dropdown cart-dropdown">
-        <a
-          href="/"
+        <div
           className="dropdown-toggle"
-          role="button"
           data-toggle="dropdown"
           aria-haspopup="true"
           aria-expanded="false"
           data-display="static"
         >
-          <span className="cart-count">{cart && cart.totalItemQuantity}</span>
-        </a>
+          {cart && cart.items.length > 0 && <span className="cart-count">{cart.totalItemQuantity}</span>}
+        </div>
 
         <div className="dropdown-menu">
           <div className="dropdownmenu-wrapper">
             <div className="dropdown-cart-header">
-              <span>{cart && cart.items.length}</span>
+              <span>{cart && cart.items.length > 0 && cart.totalItemQuantity}</span>
 
-              <a href="#" onClick={this.handleOnClick
-              }>View Cart</a>
+              <Link to="/cart" onClick={this.handleOnClick}>
+                View Cart
+              </Link>
             </div>
+
             <div className="dropdown-cart-products">
-              {cart && cart.items.map((item, i) => {
-                return (
-                  <div className="product">
-                    <div className="product-details">
-                      <h4 className="product-title">
-                        <a href="#">{item && item.title}</a>
-                      </h4>
+              {cart && Array.isArray(cart.items) && cart.items.length ? (
+                cart.items.map((item, i) => {
+                  return (
+                    <div className="product" key={i}>
+                      <div className="product-details">
+                        <h4 className="product-title">
+                          <Link to={`/product/${item.productSlug}`}>{item && item.title}</Link>
+                        </h4>
 
-                      <span className="cart-product-info">
-        <span className="cart-product-qty">{item && item.quantity}</span>x {item && item.price.displayAmount}
-      </span>
+                        <span className="cart-product-info">
+                          <span className="cart-product-qty">{item && item.quantity}</span>x{" "}
+                          {item && item.price.displayAmount}
+                        </span>
+                      </div>
+
+                      <figure className="product-image-container">
+                        <Link to={`/product/${item.productSlug}`} className="product-image">
+                          <img src={item.imageURLs.thumbnail} alt="product" />
+                        </Link>
+                        <a href="#" onClick={() => this.handleRemoveItem(item._id)} className="btn-remove">
+                          <i className="icon-cancel" />
+                        </a>
+                      </figure>
                     </div>
-
-                    <figure className="product-image-container">
-                      <a href="#" className="product-image">
-                        <img src="/static/images/products/cart/product-1.jpg" alt="product"/>
-                      </a>
-                      <a href="/" className="btn-remove" title="Remove Product">
-                        <i className="icon-cancel"/>
-                      </a>
-                    </figure>
+                  );
+                })
+              ) : (
+                <Fragment>
+                  <p style={{ textAlign: "center", marginTop: "20px" }}>Your Shopping Cart is Empty</p>
+                  <div className="dropdown-cart-action">
+                    <Link href="#" onClick={this.handleClick} className="btn btn-block">
+                      Shopping Continue
+                    </Link>
                   </div>
-                )
-              })}
+                </Fragment>
+              )}
             </div>
+            {cart && Array.isArray(cart.items) && cart.items.length > 0 && (
+              <Fragment>
+                <div className="dropdown-cart-total">
+                  <span>Total</span>
+                  <span className="cart-total-price">{cart && cart.checkout.summary.total.displayAmount}</span>
+                </div>
 
-            <div className="dropdown-cart-total">
-              <span>Total</span>
-
-              <span className="cart-total-price">{Number.parseFloat(total).toFixed(2)}</span>
-            </div>
-
-            <div className="dropdown-cart-action">
-              <button onClick={this.handleCheckoutButtonClick} className="btn btn-block">
-                Checkout
-              </button>
-            </div>
+                <div className="dropdown-cart-action">
+                  <Link to="/checkout" onClick={this.handleCheckoutButtonClick} className="btn btn-block">
+                    Checkout
+                  </Link>
+                </div>
+              </Fragment>
+            )}
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
