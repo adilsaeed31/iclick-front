83837a8182f00893a4cbcfcb3acc8fe0dcbe7a76
appwrk.global <appwrk.global@gmail.com>
2019-08-06 20:41:28 +0530

    product listing and filter design partially completed

--- src/custom/iclick/components/ProductGridHero/ProductGridHero.js
+++ src/custom/iclick/components/ProductGridHero/ProductGridHero.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 
+
+const styles = (theme) => ({
+  heroImg: {
+    width: "100%"
+  },
+  heroGridContainer: {
+    //maxWidth: theme.layout.mainContentMaxWidth,
+    margin: "12px auto"
+  }
+});
+
+@withStyles(styles, { name: "SkProductGridHero" })
 export default class ProductGridHero extends Component {
   static propTypes = {
     tag: PropTypes.object
@@ -10,20 +24,36 @@ export default class ProductGridHero extends Component {
     tag: {}
   }
 
+  // render() {
+  //   const {
+  //     tag: { heroMediaUrl }
+  //   } = this.props;
+
+  //   if (!heroMediaUrl) return null;
+
+  //   return (
+  //     <section>
+  //       <div>
+  //         <div>
+  //           <img src={heroMediaUrl} alt="Product category" />
+  //         </div>
+  //       </div>
+  //     </section>
+  //   );
+  // }
+
   render() {
-    const {
-      tag: { heroMediaUrl }
-    } = this.props;
+    const { classes, tag: { heroMediaUrl } } = this.props;
 
     if (!heroMediaUrl) return null;
 
     return (
-      <section>
-        <div>
-          <div>
-            <img src={heroMediaUrl} alt="Product category" />
-          </div>
-        </div>
+      <section className={classes.heroGridContainer}>
+        <Grid container spacing={24}>
+          {/* <Grid item xs={12}>
+            <img src={heroMediaUrl} alt="Product Category" className={classes.heroImg} />
+          </Grid> */}
+        </Grid>
       </section>
     );
   }
